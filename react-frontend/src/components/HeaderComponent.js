7e885c7f4ac74bc74e2b4921b2b2804fb34cb901
Marco Soekmono <paomarco8@gmail.com>
2023-06-08 14:36:44 -0700

    repositioned elements in navbar

--- react-frontend/src/components/HeaderComponent.js
+++ react-frontend/src/components/HeaderComponent.js
@@ -7,13 +7,14 @@ export default function HeaderComponent({ showLogin }) {
         <header>
             <nav className="navbar navbar-expand-md navbar-dark bg-dark">
                 <Spacer axis="horizontal" size={16} />
-                <span className="navbar-brand">Your Neighborhood Tool App</span>
                 <div>
                   <img src="favicon-wrench-96.png" alt="App Logo" width="85%" />
                 </div>
+                <span className="navbar-brand"><h2>Your Neighborhood Tool App</h2></span>
                 <button className="btn btn-primary sign-in-btn" onClick={showLogin}>
                   Sign in
                 </button>
+                <Spacer axis="horizontal" size={16}></Spacer>
                 <Spacer axis="vertical" size={40} />
             </nav>
         </header>
